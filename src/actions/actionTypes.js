57038069d75966150c94b97ca4374d16da5c93e4
Jose Saul Serrano <Jose Saul Serrano>
2021-12-05 15:01:32 -0500

    defined the different action types

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const actionTypes = {
+   completeTodo: "COMPLETE_TODO",
+   removeTodo: "REMOVE_TODO",
+   getAll: "GET_ALL_TODOS",
+   getCompletedOnly: "GET_COMPLETED_ONLY",
+   getActiveOnly: "GET_ACTIVE_ONLY",
+}
